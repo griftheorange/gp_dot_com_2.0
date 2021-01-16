7d6cf2c90f4e697d9771409d248a9b7c992dc8cd
griftheorange <griffinpoole5@gmail.com>
2021-01-15 19:16:00 -0500

    Reformated skills list and automated generation of JSX based on passed-in object. Pending peer review for aesthetic appeal

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -5,6 +5,79 @@ import {List, Button, Icon} from 'semantic-ui-react'
 
 import Divider from './Divider'
 
+function titleGenerator(stringVal){
+    if(stringVal.includes("&")){
+        let arr = stringVal.split("&");
+        return (
+            <h2>{arr[0]} <span>{'&'}</span> {arr[1]}</h2>
+        )
+    }
+    else{
+        return (
+            <h2>{stringVal}</h2>
+        )
+    }
+}
+
+function skillsGenerator(skillHash){
+    return(
+        <>
+            <h2>Skills</h2>
+            <Divider/>
+            <List className={'skills-list'}>
+                {Object.entries(skillHash).map((arr) => {
+                    let [key, value] = arr;
+                    return (
+                        <>
+                        {titleGenerator(key)}
+                        <Divider/>
+                        <div className="sublist">
+                            {Array.isArray(value) ? (
+                                <>
+                                {value.map((item) => {
+                                    return <div className="sublist-item">&bull; {item}</div>
+                                })}
+                                </>
+                            ) : (
+                                <>
+                                {Object.entries(value).map((arr) => {
+                                    let [key2, value2] = arr;
+                                    return (
+                                        <>
+                                        <div className="sublist-item">
+                                            {value2 != null ? (
+                                                <>
+                                                <div className="sublist-key">{key2}:</div>
+                                                <div className="sublist-values">
+                                                    {value2.map((item) => {
+                                                        return (
+                                                            <div>{item}</div>
+                                                        )
+                                                    })}
+                                                </div>
+                                                </>
+                                            ) : (
+                                                <>
+                                                <div className="sublist-key">{key2}</div>
+                                                <div className="sublist-values"></div>
+                                                </>
+                                            )}
+                                        </div>
+                                        <Divider faded={true}/>
+                                        </>
+                                    )
+                                })}
+                                </>
+                            )}
+                        </div>
+                        </>
+                    )
+                })}
+            </List>
+        </>
+    )
+}
+
 export default function(props){
     return (
         <div className='business-card-content-wrapper'>
@@ -50,10 +123,33 @@ export default function(props){
                 </div>
             </div>
             <div id='skills' className='block'>
-
+                {skillsGenerator({
+                    "Web & Software Developemnt":{
+                        "Java":["Oracle Certified Java Associate", "Spring Boot", "Spring REST", "Spring DATA/ORM/JDBC"],
+                        "C#":[".NET Core", "ASP.NET", "Entity Framework"],
+                        "Javascript":["React", "Angular", "Node.js"],
+                        "Ruby":["Rails", "Sinatra"],
+                        "Python":["Flask", "Pandas"],
+                        "SQL":["MySQL", "SQLite", "Postgresql"],
+                        "HTML":["HTML5"]
+                    },
+                    "Data Visualization & Analysis":{
+                        "Python":["Pandas", "Numpy"],
+                        "Javascript":["D3.js", "Chart.js"],
+                        "Ruby":["Active Record"]
+                    },
+                    "Soft Skills":["Strong Passion for Data Visualization", 
+                                   "Flatiron School Graduate of Software Engineering", 
+                                   "Rutgers BA in Cell Biology and Neuroscience",
+                                   "Excellent Communication Skills",
+                                   "Very Strong Organization Skills",
+                                   "Highly Personable"]
+                })}
+                
             </div>
             <div id='about-me' className='block'>
-
+                <h2>About Me</h2>
+                <Divider/>
             </div>
         </div>
     )
