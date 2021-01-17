a846f87e19d5bfb637cd011b8c94fdd1321efaff
griftheorange <griffinpoole5@gmail.com>
2021-01-17 17:56:07 -0500

    Rearranged greatly the business card, much bigger and more readable

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -8,10 +8,14 @@ import AboutMe from './BusinessCard/AboutMe'
 export default function(props){
     return (
         <div className='business-card-content-wrapper'>
-            <ProfilePic/>
-            <Contact/>
-            <Skills/>
-            <AboutMe/>
+            <div className="business-card-sub-section">
+                <ProfilePic/>
+                <AboutMe/>
+            </div>
+            <div className="business-card-sub-section">
+                <Contact/>
+                <Skills/>
+            </div>
         </div>
     )
 }
